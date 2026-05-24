25bc3cfbd05d4f059da3a1cb1b7db60a75a78b91
mmd-punisher <mohammadmoosapoor4@gmail.com>
2026-05-24 18:26:19 +0330

    feat: user and tasks model

--- core/tasks/models.py
+++ core/tasks/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tasks(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='tasks'
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name_plural = 'Tasks'
